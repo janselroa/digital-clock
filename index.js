5b414c18fb05ab52a9753da11efc470bdf9902e0
jansel <janselroa2424@gmail.com>
2022-01-08 18:51:23 -0400

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const btn = document.querySelector("button");
 const settings = document.querySelector(".settings-window")
 const p = document.querySelector("p");
+const p2 = document.querySelector(".date");
+if (localStorage.getItem("background") != null) {
+    document.body.style.backgroundImage = localStorage.getItem("background")
+}
+
 btn.addEventListener("click", () => {
     settings.classList.toggle("settings-show")
 })
@@ -8,10 +13,22 @@ settings.addEventListener("click", (e) => {
     if (e.target != settings) {
         const img = e.target.getAttribute("src");
         document.body.style.backgroundImage = `url("${img}")`
-
+        localStorage.setItem("background", `url("${img}")`)
     }
 })
+
+const daysWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+]
+const Months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 setInterval(() => {
     let date = new Date()
     p.textContent = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    p2.textContent = `${daysWeek[date.getDay()]}, ${date.getDate()} ${Months[date.getMonth()]} ${date.getFullYear()}`
 }, 300)
